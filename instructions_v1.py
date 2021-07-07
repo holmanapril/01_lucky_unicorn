528d55eabe18b19eec70a576470bbe9a28bbd4ab
holman.april <holman.april@awatapu.school.nz>
2021-07-07 14:43:54 +1200

    base from Yes_no_checker_v2 to build the instructions onto

---
+++ instructions_v1.py
@@ -0,0 +1,21 @@
+# Functions go here...
+def yes_no(question):
+    valid = False
+    while not valid:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            response = "yes"
+            return response
+
+        elif response == "no" or response == "n":
+            response = "no"
+            return response
+
+        else:
+            print("Please answer yes or no")
+
+# Main routine goes here...
+played_before = yes_no("Have you played the game before?")
+print("You chose {}".format(played_before))
+print()
