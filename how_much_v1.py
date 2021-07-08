603e53230fd67a9a610ac194d3e0a6a177c1d8af
holman.april <holman.april@awatapu.school.nz>
2021-07-09 09:56:24 +1200

    Original how_much code but will need editing to make it more efficient

---
+++ how_much_v1.py
@@ -0,0 +1,13 @@
+error = "Please enter a whole number between 1 and 10"
+
+valid = False
+while not valid:
+    # Ask question
+    how_much = int(input("How much would you like to play with? "))
+    if 0 < how_much <= 10:
+        print("You have asked to play with ${}".format(how_much))
+    else:
+        print("error")
+        print("Please enter a number between 1 and 10")
+
+
