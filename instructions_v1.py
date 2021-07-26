a648b5cf47520764d8b7603843487c9b1979cae9
holman.april <holman.april@awatapu.school.nz>
2021-07-26 13:22:18 +1200

    added .strip()

--- instructions_v1.py
+++ instructions_v1.py
@@ -2,7 +2,7 @@
 def yes_no(question):
     valid = False
     while not valid:
-        response = input(question).lower()
+        response = input(question).strip().lower()
 
         if response == "yes" or response == "y":
             response = "yes"
