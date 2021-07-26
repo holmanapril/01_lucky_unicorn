3b6148e7efd8c7b0fedb2d1de085dc1373947024
holman.april <holman.april@awatapu.school.nz>
2021-07-26 12:55:45 +1200

    Decided to fix it on the same tab so now the code works with all possible input

--- how_much_v1.py
+++ how_much_v1.py
@@ -2,12 +2,14 @@ error = "Please enter a whole number between 1 and 10\n"
 
 valid = False
 while not valid:
-    # Ask question
-    how_much = int(input("How much would you like to play with? "))
-    if 0 < how_much <= 10:
-        print("You have asked to play with ${}".format(how_much))
-        valid = True
-    else:
+    try:
+        # Ask question
+        how_much = int(input("How much would you like to play with? "))
+        if 0 < how_much <= 10:
+            print("You have asked to play with ${}".format(how_much))
+        else:
+            print(error)
+    except ValueError:
         print(error)
 
 
