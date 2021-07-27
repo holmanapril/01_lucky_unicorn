1f7d4bb8380c9044cc5dd3ae5df48eeaa90e29d0
holman.april <holman.april@awatapu.school.nz>
2021-07-27 15:07:46 +1200

    changing the tokens list variable so that the player loses money

---
+++ token_generator_v3.py
@@ -0,0 +1,22 @@
+import random
+
+# Main Routine
+
+tokens = ["unicorn", "horse", "horse", "horse", "donkey", "donkey", "donkey", "zebra", "zebra", "zebra"]
+STARTING_BALANCE = 100
+balance = STARTING_BALANCE
+# Testing Loop to generate 20 tokens
+for item in range(0, 100):
+    chosen = random.choice(tokens)
+
+    # Change balance
+    if chosen == "unicorn":
+        balance += 4
+    elif chosen == "donkey":
+        balance -= 1
+    else:
+        balance -= 0.5
+
+print()
+print("Starting Balance: ${:.2f}".format(STARTING_BALANCE))
+print("Final Balance: ${:.2f}".format(balance))
