a7bf064395ede595329b9df16dafe0bd745326d8
holman.april <holman.april@awatapu.school.nz>
2021-08-09 13:26:28 +1200

    all the different print statements for all the tokens

--- statement_decorator.py
+++ statement_decorator.py
@@ -21,3 +21,35 @@ print(unicorn_top_bottom)
 print(unicorn_token)
 print(unicorn_top_bottom)
 
+zebra_token = " You got a Zebra "
+zebra_sides = "z" * 3
+
+zebra_token = "{} {} {}".format(zebra_sides, zebra_token, zebra_sides)
+
+zebra_top_bottom = "z" * len(zebra_token)
+
+print(zebra_top_bottom)
+print(zebra_token)
+print(zebra_top_bottom)
+
+horse_token = " You got a Horse "
+horse_sides = "h" * 3
+
+horse_token = "{} {} {}".format(horse_sides, horse_token, horse_sides)
+
+horse_top_bottom = "h" * len(horse_token)
+
+print(horse_top_bottom)
+print(horse_token)
+print(horse_top_bottom)
+
+donkey_token = " You got a Donkey "
+donkey_sides = "d" * 3
+
+donkey_token = "{} {} {}".format(donkey_sides, donkey_token, donkey_sides)
+
+donkey_top_bottom = "d" * len(donkey_token)
+
+print(donkey_top_bottom)
+print(donkey_token)
+print(donkey_top_bottom)
